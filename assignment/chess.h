fb1584e880950ddb8610412be31d3b9fdd6a6064
simonHellrigl <simsi.hellrigl@gmail.com>
2018-10-30 16:41:54 +0100

    save

--- assignment/chess.h
+++ assignment/chess.h
@@ -39,15 +39,16 @@ typedef ChessSquare ChessBoard[8][8];
 
   bool is_square_occupied(ChessBoard chess_board, File file,  Rank rank);
   struct ChessSquare* get_square(ChessBoard chess_board,File file,  Rank rank);
-  bool add_piece(ChessBoard chess_board, File file ,int rank,struct ChessPiece piece);
+  bool add_piece(ChessBoard chess_board, File file ,int rank,struct ChessPiece chess_piece);
   struct ChessPiece get_piece(ChessBoard chess_board, File file , Rank rank);
-  bool remove_piece(ChessBoard chess_board, char Xpos ,int Ypos);
+  bool remove_piece(ChessBoard chess_board, File file, Rank rank);
   bool is_piece(struct ChessPiece piece, enum Color color ,enum PieceType type);
-  bool squares_share_rank(File file1,  Rank rank1, File file2,  Rank rank2);
- bool squares_share_file(File file1,  Rank rank1, File file2,  Rank rank2);
- bool squares_share_diagonal(File file1,  Rank rank1, File file2,  Rank rank2);
 
- bool squares_share_knights_move(File file1, Rank rank1, File file2,  Rank rank2);
- bool squares_share_kings_move(File file1,  Rank rank1, File file2,  Rank rank2);
- bool squares_share_pawns_move(enum Color color, NormalMove, File file1,  Rank rank1, File file2,  Rank rank2);
- bool squares_share_queens_move(File file1,  Rank rank1, File file2,  Rank rank2);
+  bool squares_share_rank(File file1,  Rank rank1, File file2,  Rank rank2);
+  bool squares_share_file(File file1,  Rank rank1, File file2,  Rank rank2);
+  bool squares_share_diagonal(File file1,  Rank rank1, File file2,  Rank rank2);
+
+  bool squares_share_knights_move(File file1, Rank rank1, File file2,  Rank rank2);
+  bool squares_share_kings_move(File file1,  Rank rank1, File file2,  Rank rank2);
+  bool squares_share_pawns_move( File file1,  Rank rank1, File file2,  Rank rank2);
+  bool squares_share_queens_move(File file1,  Rank rank1, File file2,  Rank rank2);
